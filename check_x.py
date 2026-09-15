3e5c859c082b447eb5fc2e3c77a90ea4d805bae7
majyeuem2542004 <tranvietbg254@gmail.com>
2026-09-15 18:15:25 +0700

    Untrack python helper scripts and add *.py to .gitignore

--- check_x.py
+++
@@ -1,9 +0,0 @@
-import os
-from PIL import Image
-
-dir_path = r'D:\game\Bla_mobile_map\ExportedProject\Assets\Texture2D'
-for f in ['X.png', 'X_02.png', 'X_1.png']:
-    p = os.path.join(dir_path, f)
-    if os.path.exists(p):
-        img = Image.open(p)
-        print(f"{f}: size={img.size}")
--- find_close_btn.py
+++
@@ -1,9 +0,0 @@
-import os
-from PIL import Image
-
-dir_path = r'D:\game\Android2D_Blas\Assets\Brotherhood\Resources\UI\Sprites'
-for f in os.listdir(dir_path):
-    if f.startswith('inventory-spritesheet_') and f.endswith('.png'):
-        p = os.path.join(dir_path, f)
-        img = Image.open(p)
-        print(f"{f}: size={img.size}, mode={img.mode}")
